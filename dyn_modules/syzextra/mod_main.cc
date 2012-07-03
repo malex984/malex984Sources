aa502a01000a34b35608d3ba6b8a40678b867360
Oleksandr Motsak <motsak@mathematik.uni-kl.de>
2012-07-03 18:30:41 +0200

    introduced syzextra.{h,cc} files for cleaner separation of algorithms and interpreter wrappers

--- dyn_modules/syzextra/mod_main.cc
+++ dyn_modules/syzextra/mod_main.cc
@@ -29,6 +29,7 @@
 #include "singularxx_defs.h"
 #include "DebugPrint.h"
 #include "myNF.h"
+#include "syzextra.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -50,6 +51,7 @@ extern int rGetISPos(const int p, const ring r);
 
 USING_NAMESPACE( SINGULARXXNAME :: DEBUG )
 USING_NAMESPACE( SINGULARXXNAME :: NF )
+USING_NAMESPACE( SINGULARXXNAME :: SYZEXTRA )
 
 
 BEGIN_NAMESPACE_NONAME

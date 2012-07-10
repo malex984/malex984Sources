1b0d4df4d129f23c9e8781b2e72b52aee6a61ae9
Oleksandr Motsak <motsak@mathematik.uni-kl.de>
2012-07-10 18:08:24 +0200

    moving/adding/changing bits of documentation

--- dyn_modules/syzextra/mod_main.cc
+++ dyn_modules/syzextra/mod_main.cc
@@ -528,7 +528,6 @@ static BOOLEAN _Compute2LeadingSyzygyTerms(leftv res, leftv h)
 
 
 
-/// TODO: save shortcut (syz: |-.->) LM(LM(m) * "t") -> syz?
 /// proc SSFindReducer(def product, def syzterm, def L, def T, list #)
 static BOOLEAN _FindReducer(leftv res, leftv h)
 {
@@ -732,7 +731,6 @@ static BOOLEAN _SchreyerSyzygyNF(leftv res, leftv h)
 
 
 
-/// TODO: save shortcut (syz: |-.->) LM(m) * "t" -> ?
 /// proc SSReduceTerm(poly m, def t, def syzterm, def L, def T, list #)
 static BOOLEAN _ReduceTerm(leftv res, leftv h)
 {
@@ -877,7 +875,6 @@ static BOOLEAN _ReduceTerm(leftv res, leftv h)
 
 
 
-// TODO: store m * @tail -.-^-.-^-.--> ?
 // proc SSTraverseTail(poly m, def @tail, def L, def T, list #)
 static BOOLEAN _TraverseTail(leftv res, leftv h)
 {
@@ -981,8 +978,8 @@ static BOOLEAN _TraverseTail(leftv res, leftv h)
 }
 
 
-// module (N, LL, TT) = SSComputeSyzygy(L, T);
-// Compute Syz(L ++ T) = N = LL ++ TT
+/// module (LL, TT) = SSComputeSyzygy(L, T);
+/// Compute Syz(L ++ T) = N = LL ++ TT
 // proc SSComputeSyzygy(def L, def T)
 static BOOLEAN _ComputeSyzygy(leftv res, leftv h)
 {
